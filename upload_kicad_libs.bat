63eabec2c7c0a4c60376051beaa7312472b06b33
Maciej Suminski <maciej.suminski@cern.ch>
2020-03-10 10:46:15 +0100

    Fixed a path in upload_kicad_libs.bat

--- upload_kicad_libs.bat
+++ upload_kicad_libs.bat
@@ -9,7 +9,7 @@ move G:\Applications\KiCad\PcbLib G:\Applications\KiCad\PcbLib.old
 move G:\Applications\KiCad\PcbLib.new G:\Applications\KiCad\PcbLib
 
 mkdir G:\Applications\KiCad\SchLib.new
-xcopy altium_converter\dblib-converter\output G:\Applications\KiCad\SchLib.new /S /EXCLUDE:upload_exclude.txt
+xcopy dblib-converter\output G:\Applications\KiCad\SchLib.new /S /EXCLUDE:upload_exclude.txt
 rmdir /s /q G:\Applications\KiCad\SchLib.old
 move G:\Applications\KiCad\SchLib G:\Applications\KiCad\SchLib.old
 move G:\Applications\KiCad\SchLib.new G:\Applications\KiCad\SchLib
